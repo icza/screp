247bb8417c17414e19e3822c055440b7806032f8
Andras Belicza <iczaaa@gmail.com>
2017-08-04 12:39:37 +0200

    Fixed typo.

--- cmd/screp/screp.go
+++ cmd/screp/screp.go
@@ -100,5 +100,5 @@ func printUsage() {
 	fmt.Println("Usage:")
 	name := os.Args[0]
 	fmt.Printf("\t%s [FLAGS] repfile.rep\n", name)
-	fmt.Println("\tRun with '-h' to see a a list of available flags.")
+	fmt.Println("\tRun with '-h' to see a list of available flags.")
 }
