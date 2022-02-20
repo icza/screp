d878b2e907df408d2e9c173788962f6ccb6de0c4
Andras Belicza <iczaaa@gmail.com>
2022-02-20 15:14:12 +0100

    Add mapDataHash cli argument

--- cmd/screp/screp.go
+++ cmd/screp/screp.go
@@ -7,11 +7,18 @@ a StarCraft: Brood War replay passed as a CLI argument.
 package main
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"hash"
 	"os"
 	"runtime"
+	"strings"
 
 	"github.com/icza/screp/rep"
 	"github.com/icza/screp/repparser"
@@ -19,22 +26,32 @@ import (
 
 const (
 	appName    = "screp"
-	appVersion = "v1.5.1"
+	appVersion = "v1.6.0"
 	appAuthor  = "Andras Belicza"
 	appHome    = "https://github.com/icza/screp"
 )
+
+const (
+	ExitCodeMissingArguments         = 1
+	ExitCodeFailedToParseReplay      = 2
+	ExitCodeFailedToCreateOutputFile = 3
+	ExitCodeInvalidMapDataHash       = 4
+)
+
+const validMapDataHashes = "valid values are 'sha1', 'sha256', 'sha512', 'md5'"
 
 // Flag variables
 var (
 	version = flag.Bool("version", false, "print version info and exit")
 
-	header    = flag.Bool("header", true, "print replay header")
-	mapData   = flag.Bool("map", false, "print map data")
-	mapTiles  = flag.Bool("maptiles", false, "print map data tiles; valid with 'map'")
-	mapResLoc = flag.Bool("mapres", false, "print map data resource locations (minerals and geysers); valid with 'map'")
-	cmds      = flag.Bool("cmds", false, "print player commands")
-	computed  = flag.Bool("computed", true, "print computed / derived data")
-	outFile   = flag.String("outfile", "", "optional output file name")
+	header      = flag.Bool("header", true, "print replay header")
+	mapData     = flag.Bool("map", false, "print map data")
+	mapTiles    = flag.Bool("maptiles", false, "print map data tiles; valid with 'map'")
+	mapResLoc   = flag.Bool("mapres", false, "print map data resource locations (minerals and geysers); valid with 'map'")
+	cmds        = flag.Bool("cmds", false, "print player commands")
+	computed    = flag.Bool("computed", true, "print computed / derived data")
+	mapDataHash = flag.String("mapDataHash", "", "calculate and print the hash of map data section using the given algorithm;\n"+validMapDataHashes)
+	outFile     = flag.String("outfile", "", "optional output file name")
 
 	indent = flag.Bool("indent", true, "use indentation when formatting output")
 )
@@ -50,17 +67,49 @@ func main() {
 	args := flag.Args()
 	if len(args) < 1 {
 		printUsage()
-		os.Exit(1)
+		os.Exit(ExitCodeMissingArguments)
 	}
 
-	r, err := repparser.ParseFile(args[0])
+	cfg := repparser.Config{
+		Commands: true,
+		MapData:  true,
+	}
+
+	var mapDataHasher hash.Hash
+	if *mapDataHash != "" {
+		cfg.Debug = true
+		switch strings.ToLower(*mapDataHash) {
+		case "md5":
+			mapDataHasher = md5.New()
+		case "sha1":
+			mapDataHasher = sha1.New()
+		case "sha256":
+			mapDataHasher = sha256.New()
+		case "sha512":
+			mapDataHasher = sha512.New()
+		default:
+			fmt.Printf("Invalid mapDataHash: %v\n", *mapDataHash)
+			fmt.Println(validMapDataHashes)
+			os.Exit(ExitCodeInvalidMapDataHash)
+		}
+	}
+
+	r, err := repparser.ParseFileConfig(args[0], cfg)
 	if err != nil {
 		fmt.Printf("Failed to parse replay: %v\n", err)
-		os.Exit(2)
+		os.Exit(ExitCodeFailedToParseReplay)
 	}
+
+	// custom holds any custom data we want in the output and is not part of rep.Replay
+	custom := map[string]interface{}{}
 
 	if *computed {
 		r.Compute()
+	}
+
+	if mapDataHasher != nil {
+		mapDataHasher.Write(r.MapData.Debug.Data)
+		custom["mapDataHash"] = hex.EncodeToString(mapDataHasher.Sum(nil))
 	}
 
 	// Zero values in replay the user do not wish to see:
@@ -90,7 +139,7 @@ func main() {
 		fp, err := os.Create(*outFile)
 		if err != nil {
 			fmt.Printf("Failed to create output file: %v\n", err)
-			os.Exit(3)
+			os.Exit(ExitCodeFailedToCreateOutputFile)
 		}
 		defer func() {
 			if err := fp.Close(); err != nil {
@@ -103,7 +152,18 @@ func main() {
 	if *indent {
 		enc.SetIndent("", "  ")
 	}
-	enc.Encode(r)
+
+	var valueToEncode interface{} = r
+
+	// If there are custom data, wrap (embed) the replay in a struct that holds the custom data too:
+	if len(custom) > 0 {
+		valueToEncode = struct {
+			*rep.Replay
+			Custom map[string]interface{}
+		}{r, custom}
+	}
+
+	enc.Encode(valueToEncode)
 }
 
 func printVersion() {
